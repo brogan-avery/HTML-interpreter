abc767e1e608d5b06fe2fb52a67e686be1b3f0cc
Ty Hobbs <43939086+tjhobbs1@users.noreply.github.com>
2021-04-06 12:06:51 -0500

    Start of App
    Start of App

---
+++ cmake-build-debug/CMakeFiles/HTML_interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HTML_interpreter.dir/main.cpp.o"
+  "HTML_interpreter"
+  "HTML_interpreter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HTML_interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**************************************************************
+* Title: HTML_interpreter
+* Author:  HTML Group
+* Created : 2021-04-01
+* Course: CIS 164- Advanced C++
+* App Description : This app will take a plain text and convert into HTML tags. Anytime it sees
+ * a ** it will turn the text after it into a tag. This app will make, making simple
+ * webpages much easier than using standard HTML elements.
+*
+***************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
